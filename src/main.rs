0af7351cd9ac154970c41f74a71dac8c4e4f3216
Grant Lemons <grant.lemons@students.alcuinschool.org>
2022-10-05 15:17:59 -0500

    fix issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32;
+
 fn main() {
     print!("a: ");
     let a: f32 = text_io::read!("{}\n");
@@ -7,21 +9,25 @@ fn main() {
 
     let interval = b - a;
     let subintervals = 5;
-    let subinterval_width = interval / subintervals;
+    let subinterval_width = interval / subintervals as f32;
 
     // calculate sum
-    let sum = 0;
+    let mut sum: f32 = 0.0;
     for i in 0..subintervals {
         sum += trapezoid_rule(
-            a + (i * subinterval_width),
-            a + ((i + 1) * subinterval_width),
+            a + ((i as f32) * subinterval_width),
+            a + ((i + 1) as f32 * subinterval_width),
             3,
         );
     }
+
+    // output
+    println!("Integral: {}", sum)
 }
 
 fn f(x: f32) -> f32 {
-    (13 / f32::sqrt(2 * f32::consts::PI)) * f23::consts::E ^ (-(1 / 2) * x ^ 2)
+    (13_f32 / f32::sqrt(2_f32 * f32::consts::PI))
+        * f32::powf(f32::consts::E, -(1_f32 / 2_f32) * f32::powi(x, 2))
 }
 
 fn f_prime(x: f32) -> f32 {
@@ -33,14 +39,14 @@ fn trapezoid_rule(a: f32, b: f32, threshold: u8) -> f32 {
     let left = f_prime(a);
     let right = f_prime(b);
     let width = b - a;
-    let avg_slope = i32::abs((right - left) / width);
+    let avg_slope = f32::abs((right - left) / width);
 
     // recursively divide
-    if (avg_slope >= threshold) {
-        trapezoid_rule(a, b - (height / 2), threshold)
-            + trapezoid_rule(a + (height / 2), b, threshold)
+    if avg_slope >= threshold.into() {
+        return trapezoid_rule(a, b - (width / 2.0), threshold)
+            + trapezoid_rule(a + (width / 2.0), b, threshold);
     }
 
     // calculate trapezoidal area
-    (height / 2) * (left + right)
+    (width / 2.0) * (left + right)
 }
